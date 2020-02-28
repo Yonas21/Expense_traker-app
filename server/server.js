645538104e40b35212897f21c1da0cfb38bc6ea8
Yonas Alem <yonasalem056@gmail.com>
2020-02-28 22:37:30 +0300

    format numbers with commas and build for production

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const path = require('path');
+
 
 // import the instance of database connection
 const connectDB = require('../config/db');
@@ -21,6 +24,11 @@ const transactionRouter = require('./routes/transaction');
 app.use(morgan('dev'));
 app.use(express.json()); // allows to use body-parser
 app.use('/api/transactions', transactionRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => res.sendFile(__dirname, 'build','index.html'));
+}
 
 const PORT = process.env.PORT || 4000;
 
